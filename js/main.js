a0db6691db41d4b58adf45eec02ed428cb1bb633
Mikhail Sokulsky <super.mixa25@gmail.com>
2019-01-13 05:04:37 +0200

    Создал скелет генератора пароля

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function generate_plain() {
+    var pass = "";
+    var strong = 8;
+    var dic = "123456789";
+
+    for (var i = 0; i < strong; i++)
+    {
+        pass += dic.charAt(Math.floor(Math.random() * dic.length));
+    }
+    document.getElementById('random').innerHTML = pass;
+}
+
+function generatep_safe() {
+    var pass = "";
+    var strong = 10;
+    var dic = "123456789BCDFGHJKLMNPQRSTVWXYZ";
+
+    for (var i = 0; i < strong; i++)
+    {
+        pass += dic.charAt(Math.floor(Math.random() * dic.length));
+    }
+    document.getElementById('random').innerHTML = pass;
+}
+
+function generatep_ultra() {
+    var pass = "";
+    var strong = 15;
+    var dic = "123456789BCDFGHJKLMNPQRSTVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (var i = 0; i < strong; i++)
+    {
+        pass += dic.charAt(Math.floor(Math.random() * dic.length));
+    }
+    document.getElementById('random').innerHTML = pass;
+}
